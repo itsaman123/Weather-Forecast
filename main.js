9d09b3e52d66ccebfb5dcf196b458c34a021a870
Aman Murya <m.amanmaurya49@gmail.com>
2022-10-15 01:33:38 +0530

    some more ui changes

--- main.js
+++ main.js
@@ -9,18 +9,21 @@ function GetInfo() {
   )
     .then((response) => response.json())
     .then((data) => {
+      //to find the the min and max values for each day
       for (i = 0; i < 5; i++) {
         document.getElementById('day' + (i + 1) + 'Min').innerHTML =
           'Min: ' +
           Number(data.list[i].main.temp_min - 273.15).toFixed(1) +
           '°';
       }
+
       for (i = 0; i < 5; i++) {
         document.getElementById('day' + (i + 1) + 'Max').innerHTML =
           'Max: ' +
           Number(data.list[i].main.temp_max - 273.15).toFixed(2) +
           '°';
       }
+      //to find Weather Icons
       for (i = 0; i < 5; i++) {
         document.getElementById('img' + (i + 1)).src =
           'http://openweathermap.org/img/wn/' +
@@ -35,6 +38,7 @@ function DefaultScreen() {
   document.getElementById('cityInput').defaultValue = 'Delhi';
   GetInfo();
 }
+//to find and displaying the text for the upcoming five days of the week
 var d = new Date();
 var weekday = [
   'Sunday',
@@ -45,6 +49,7 @@ var weekday = [
   'Friday',
   'Saturday',
 ];
+//Function to get the correct integer for the index of the days array
 function CheckDay(day) {
   if (day + d.getDay() > 6) {
     return day + d.getDay() - 7;
